636866d00f051240de4647ee1ebc63dfbada8bd0
Carlos Barrera <carlostonatihu@gmail.com>
2018-03-10 12:57:11 -0600

    Vistas basicas

--- backendninja/src/main/java/com/tona/backendninja/controller/HelloWorldController.java
+++ backendninja/src/main/java/com/tona/backendninja/controller/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.tona.backendninja.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author tona created on 09/03/2018 for backendninja.
@@ -10,8 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/say")
 public class HelloWorldController {
+    private static final String EXAMPLE_VIEW = "example";
+
+    // Primera forma (para redirecciones)
+    @RequestMapping(value="/helloString", method = RequestMethod.GET)
+    public String exampleString(Model model) {
+        model.addAttribute("name", "General Kenobi!");
+        return EXAMPLE_VIEW;
+    }
+
+    // Segunda forma (cuando se deben insertar muchos datos)
+    @RequestMapping(value="/helloModelView", method = RequestMethod.GET)
+    public ModelAndView exampleMV() {
+        ModelAndView modelAndView = new ModelAndView(EXAMPLE_VIEW);
+        modelAndView.addObject("name", "there!");
+        return modelAndView;
+    }
+
+    // Tercera forma, tambien aplica a ModelAndView
     @GetMapping("/hello")
-    public String helloWorld() {
-        return "hello-world";
+    public String helloWorld(Model model) {
+        model.addAttribute("name", "there!");
+        return EXAMPLE_VIEW;
     }
 }
