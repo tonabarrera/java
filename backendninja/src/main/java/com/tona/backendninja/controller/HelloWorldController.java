bb431274d7358754bb69475cf4c80a0a3fd8562d
Carlos Barrera <carlostonatihu@gmail.com>
2018-03-10 13:17:39 -0600

    Iterar listas en el template

--- backendninja/src/main/java/com/tona/backendninja/controller/HelloWorldController.java
+++ backendninja/src/main/java/com/tona/backendninja/controller/HelloWorldController.java
@@ -1,11 +1,15 @@
 package com.tona.backendninja.controller;
 
+import com.tona.backendninja.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author tona created on 09/03/2018 for backendninja.
@@ -18,7 +22,7 @@ public class HelloWorldController {
     // Primera forma (para redirecciones)
     @RequestMapping(value="/helloString", method = RequestMethod.GET)
     public String exampleString(Model model) {
-        model.addAttribute("name", "General Kenobi!");
+        model.addAttribute("people", getPeople());
         return EXAMPLE_VIEW;
     }
 
@@ -26,14 +30,25 @@ public class HelloWorldController {
     @RequestMapping(value="/helloModelView", method = RequestMethod.GET)
     public ModelAndView exampleMV() {
         ModelAndView modelAndView = new ModelAndView(EXAMPLE_VIEW);
-        modelAndView.addObject("name", "there!");
+        modelAndView.addObject("people", getPeople());
         return modelAndView;
     }
 
     // Tercera forma, tambien aplica a ModelAndView
     @GetMapping("/hello")
     public String helloWorld(Model model) {
-        model.addAttribute("name", "there!");
+        model.addAttribute("people", getPeople());
         return EXAMPLE_VIEW;
+    }
+
+    private List<Person> getPeople() {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("General Kenobi",30));
+        people.add(new Person("Griveous",10));
+        people.add(new Person("Anakin",20));
+        people.add(new Person("Palpatine",80));
+        people.add(new Person("R2D2",2));
+
+        return people;
     }
 }
---
+++ backendninja/src/main/java/com/tona/backendninja/model/Person.java
@@ -0,0 +1,30 @@
+package com.tona.backendninja.model;
+
+/**
+ * @author tona created on 10/03/2018 for backendninja.
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
