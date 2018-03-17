62c5c391dc1f673644f8488245fc5953e071f09a
Carlos Barrera <carlostonatihu@gmail.com>
2018-03-16 21:26:03 -0600

    Otro proyecto

---
+++ backendninja/src/main/java/com/tona/backendninja/model/UserCredential.java
@@ -0,0 +1,33 @@
+package com.tona.backendninja.model;
+
+/**
+ * @author tona created on 14/03/2018 for backendninja.
+ */
+public class UserCredential {
+    private String username;
+    private String password;
+
+    public UserCredential(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public UserCredential() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
